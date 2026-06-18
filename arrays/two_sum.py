7c26afd7b6d97afd6dabd7938e87bf27b3d05679
Lochan <lochan1410a@gmail.com>
2026-06-19 00:51:02 +0530

    solved my 1st leet_code problem
    Implement brute force solution for Two Sum problem.

---
+++ arrays/two_sum.py
@@ -0,0 +1,17 @@
+# Problem: Two Sum (LeetCode #1)
+# Difficulty: Easy
+# Topic: Arrays
+
+# Approach (Brute Force):
+# Check every pair of elements
+# If sum equals target → return indices
+
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
+
+class Solution:
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
